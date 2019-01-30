6b1c4ac66df8b7c6b268b9a845e5c5042f42a5d3
sy2729 <sy2729@tc.columbia.edu>
2019-01-30 17:31:06 +0800

    Updates

---
+++ precache-manifest.4b24590fcea581d65f201dc6d32a3d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c40e7975498a24b7718",
+    "url": "/wechat-conversation-simulator/static/css/main.53d78185.chunk.css"
+  },
+  {
+    "revision": "0c40e7975498a24b7718",
+    "url": "/wechat-conversation-simulator/static/js/main.0c40e797.chunk.js"
+  },
+  {
+    "revision": "00750e2df05141866981",
+    "url": "/wechat-conversation-simulator/static/css/1.8c42a3dc.chunk.css"
+  },
+  {
+    "revision": "00750e2df05141866981",
+    "url": "/wechat-conversation-simulator/static/js/1.00750e2d.chunk.js"
+  },
+  {
+    "revision": "5ce7b372f2bd91760f33",
+    "url": "/wechat-conversation-simulator/static/js/runtime~main.5ce7b372.js"
+  },
+  {
+    "revision": "16ad4eb4227893678b276a7db2a7b682",
+    "url": "/wechat-conversation-simulator/index.html"
+  }
+];
